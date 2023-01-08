d2e7570ad59900398af74ae7e0611662f258a8b2
Nathan Istace <nathan.istace@gmail.com>
2023-01-08 18:56:35 +0100

    Intro screen

---
+++ Assets/Scripts/LD52/Scenes/MenuScene/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils.Extensions;
+
+namespace LD52.Scenes {
+	public class MenuController : MonoBehaviour {
+		[SerializeField] protected MenuUi _menuUi;
+
+		private void Start() {
+			_menuUi.onClick.AddListenerOnce(() => SceneManager.LoadScene("Roguelike"));
+		}
+	}
+}
---
+++ Assets/Scripts/LD52/Scenes/MenuScene/MenuUi.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using LD52.Assets;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Utils.Extensions;
+
+public class MenuUi : MonoBehaviour {
+	[SerializeField] protected Image[]  _images;
+	[SerializeField] protected string[] _names;
+	[SerializeField] protected Button   _startButton;
+
+	private Dictionary<string, Sprite[]> danceSequences { get; } = new Dictionary<string, Sprite[]>();
+
+	public UnityEvent onClick => _startButton.onClick;
+
+	private void Start() {
+		danceSequences.SetAll(_names.Select(
+				t => (t, new[] { AssetLibrary.vegetableSheet[$"{t}.dance.000"], AssetLibrary.vegetableSheet[$"{t}.dance.001"], AssetLibrary.vegetableSheet[$"{t}.dance.002"] }))
+			.ToDictionary(t => t.t, t => t.Item2));
+	}
+
+	private void Update() {
+		for (var i = 0; i < _images.Length; ++i) {
+			_images[i].sprite = danceSequences[_names[i]][Mathf.FloorToInt(Time.time * 3) % 3];
+		}
+	}
+}

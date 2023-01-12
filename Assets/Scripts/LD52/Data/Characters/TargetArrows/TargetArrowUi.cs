32a7ba182ad95877313a756b239a2db6f902e7a5
Nathan Istace <nathan.istace@gmail.com>
2023-01-12 09:10:47 +0100

    [1.2] fixed arrow length to work on all screen resolutions

--- Assets/Scripts/LD52/Data/Characters/TargetArrows/TargetArrowUi.cs
+++ Assets/Scripts/LD52/Data/Characters/TargetArrows/TargetArrowUi.cs
@@ -10,14 +10,17 @@ public class TargetArrowUi : MonoBehaviourUi {
 	private const float offset = 40;
 	private const float min    = 20;
 
-	[SerializeField] protected Image _renderer;
-	[SerializeField] protected float _defaultOpacity = .5f;
-	[SerializeField] protected float _hoveredOpacity = 1f;
-	[SerializeField] protected bool  _hovered;
+	[SerializeField] protected Image  _renderer;
+	[SerializeField] protected float  _defaultOpacity = .5f;
+	[SerializeField] protected float  _hoveredOpacity = 1f;
+	[SerializeField] protected bool   _hovered;
+	[SerializeField] protected Canvas _parentCanvas;
+
+	private Canvas parentCanvas => _parentCanvas ? _parentCanvas : _parentCanvas = GetComponentInParent<Canvas>();
 
 	public IEnumerator Reveal(GenericCharacter target) {
 		transform.offsetMin = transform.offsetMin.With(y: 0);
-		transform.offsetMax = transform.offsetMax.With(y: Mathf.Max(min, Vector2.Distance(transform.position, BattleUiData.uiPerCharacter[target].position) - offset));
+		transform.offsetMax = transform.offsetMax.With(y: Mathf.Max(min, Vector2.Distance(transform.position, BattleUiData.uiPerCharacter[target].position) - offset) / parentCanvas.scaleFactor);
 		transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.up, BattleUiData.uiPerCharacter[target].position - (Vector2)transform.position));
 		_renderer.color = _renderer.color.With(a: 0);
 		SetHovered(false);
